08b563bf79ec0a5c8b5da7d32ada044c4f75073e
bkillenit <blaine.j.killen@gmail.com>
2014-12-26 19:26:09 -0600

    Initial setup of project. Added .gitignore and requirements.txt files and ran syncdb

--- smarthumidor/settings.py
+++ smarthumidor/settings.py
@@ -28,15 +28,24 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
-INSTALLED_APPS = (
+# Application definition
+DEFAULT_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles'
 )
+
+THIRD_PARTY_APPS = ()
+
+LOCAL_APPS = (
+    'rest_framework',
+    'jquery'
+)
+
+INSTALLED_APPS = DEFAULT_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
