4e7d2b078bf241a3e8ef25761f1eef3a34910bbb
bkillenit <blaine.j.killen@gmail.com>
2015-01-07 23:57:55 -0600

    integrated air pump voltage control based on desired humidity

--- humidor/humidor.ino
+++ humidor/humidor.ino
@@ -1,7 +1,7 @@
 #include <Servo.h>
 #include "DHT.h"
 #include <SPI.h>
-#include <Ethernet.h>
+#include <EthernetV2_0.h> // custom ethernet library for the seeed eth shield v2
 #define DHTTYPE DHT11   // DHT 11 sensor type 
 #define DHTPIN 2 
 
@@ -10,31 +10,73 @@ int buttonClicked;
 boolean lidOpen;
 int degreesRotation = 22; //sets the initial degrees of rotation for the servo to begin at
 int delayHumiditySensor;
+
+// these are test values that will be dynamically set from the web in later versions
+float desiredHumidity = 40;
+const int humidityTolerance = 5;
+
 float hi;
 
-// MAC address from Ethernet shield sticker under board
-byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-IPAddress ip(10, 0, 0, 20); // IP address, may need to change depending on network
+// Initialization of internet configuration constants
+// Enter a MAC address for your controller below.
+// Newer Ethernet shields have a MAC address printed on a sticker on the shield
+byte mac[] = { 0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02 };
 EthernetServer server(80);  // create a server at port 80
+EthernetClient client;
+#define W5200_CS  10
+#define SDCARD_CS 4
 
-//initialize connected pins for I/O  
-int buttonPin = 7;
-int DHTPin = 2;
-int servoPin = 9;
+//initialize connected pins for I/O
+// Note: pins 10, 11, 12, and 13 are 
+const int buttonPin = 7;
+const int DHTPin = 2;
+const int servoPin = 9;
+const int pumpPin = 6;
+
+// initializing the analog values for high and low voltages that control the air pump
+// the numbers are merely test numbers and have not been calibrated yet
+const int highVoltage = 250;
+const int lowVoltage = 210;
 
 DHT dht(DHTPIN, DHTTYPE);
 
+// using an array instead of a more explicit struct because Arduino makes prototypes for functions before 
+// it generates custom structs.This keeps us from passing structs in to functions.
+// an explanation for this problem can be found at http://forum.arduino.cc/index.php?topic=41848.0
+// index structure of the array is: [0] = tempC, [1] = tempF, [2] = humidityLevel, [3] = heat index
+float weatherData[4] = {};
+
 void setup() {
-  Ethernet.begin(mac, ip);  // initialize Ethernet device
-  server.begin();           // start to listen for clients
+  Serial.begin(9600);
+    
+  // start the Ethernet connection:
+//  if (Ethernet.begin(mac) == 0) {
+//    Serial.println("Failed to configure Ethernet using DHCP");
+//    // no point in carrying on, so do nothing forevermore:
+//    for(;;)
+//      ;
+//  }
+//  
+//  // print your local IP address:
+//  Serial.print("My IP address: ");
+//  for (byte thisByte = 0; thisByte < 4; thisByte++) {
+//    // print the value of each byte of the IP address:
+//    Serial.print(Ethernet.localIP()[thisByte], DEC);
+//    Serial.print("."); 
+//  }
+//  Serial.println();
+  
+  // start to listen for clients once the web server is running and configured
+//  server.begin();   
   
   delayHumiditySensor++;
   
-  myservo.attach(servoPin);  // attaches the servo on pin 11 to the servo object 
+  myservo.attach(servoPin);  // attaches the servo on pin 9 to the servo object 
   myservo.write(degreesRotation);
   pinMode(buttonPin, INPUT);
+  pinMode(pumpPin, OUTPUT);
+  analogWrite(pumpPin, lowVoltage);
   
-  Serial.begin(9600);
   Serial.println("DHTxx test!");
   dht.begin();
 } 
@@ -49,25 +91,21 @@ void loop() {
     }
   }  
   
-  // constructing a blank array and passing in a pointer to the first address to the readWeatherData method
-  float weatherData[4] = {};
   readWeatherData( weatherData );
-    
-//  if(*weatherData != NULL) {
-//    Serial.println("'{\"weather\": { \"temp\": \"");
-//    Serial.print(weatherData[0]);
-//    Serial.print("\", \"tempf\": \"");
-//    Serial.print(weatherData[1]);
-//    Serial.print("\", \"humidity\": \"");
-//    Serial.print(weatherData[2]);
-//    Serial.print("\", \"heatindex\": \"");
-//    Serial.print(weatherData[3]);
-//    Serial.print("\"} }'");
-//  }
-  Serial.println("weather Data outside of scope is");
-  Serial.println(weatherData[2]);
-    
-  // listen for incoming clients
+  
+  float currentHumidity = weatherData[2];
+  
+  // if the current humidity is less than the desired humidity accounting for tolerance,
+  // switch the motor pump's voltage to high resulting in higher air flow rate
+  if( currentHumidity < (desiredHumidity - humidityTolerance) ) {
+      analogWrite(pumpPin, highVoltage);
+      while(currentHumidity != (desiredHumidity + humidityTolerance)) {
+          ;
+      }
+      analogWrite(pumpPin, lowVoltage);
+  }
+        
+//  // listen for incoming clients
 //  EthernetClient client = server.available();
 //  if (client) {
 //    // an http request ends with a blank line
@@ -84,26 +122,26 @@ void loop() {
 //          client.println("Content-Type: application/json");
 //          client.println();
 //          
-//          // attempts to read the weather data from the sensor until it returns valid data
-//          float *weatherData = readWeatherData();
+//          client.println("<html><p>Hello World!</p></html>");  
+//          
+//          // constructing a blank array and passing in a pointer to the first address to the readWeatherData method
+//          float weatherData[4] = {};
+//          readWeatherData( weatherData );
 //          
 //          while(weatherData == NULL) {
-//            weatherData = readWeatherData;
+//            readWeatherData( weatherData );          
 //          }
 //          
-//          client.println("{'weather': { 'temp': ");
-//          client.println(weatherData[0]);
-//          client.println(", tempf: ");
-//          client.println(weatherData[1]);
-//          client.println(", humidity: ");
-//          client.println(weatherData[2]);
-//          client.println(", heatindex: ");
-//          client.println(weatherData[3]);
-//          client.println("} }");
-//            
-//          if(weatherData) {
-//            free(weatherData);
-//          }
+//          // respond to the JSON request using JSON-P extension
+////          client.println("{'weather': { 'temp': ");
+////          client.println(weatherData[0]);
+////          client.println(", tempf: ");
+////          client.println(weatherData[1]);
+////          client.println(", humidity: ");
+////          client.println(weatherData[2]);
+////          client.println(", heatindex: ");
+////          client.println(weatherData[3]);
+////          client.println("} }");
 //          
 //          break;
 //        }
