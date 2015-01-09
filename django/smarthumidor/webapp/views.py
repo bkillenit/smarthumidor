72274a4164e6bf7f2dacc2ea63c0748660bd88f4
bkillenit <blaine.j.killen@gmail.com>
2015-01-08 22:17:22 -0600

    added web server capabilities to the arduino. now serves the weather data to a browser. also modified humidity logic and blocks used which allowed the air pump to be controlled concurrently with the rest of the program

---
+++ django/smarthumidor/webapp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+import json
+import httplib2
+
+
+# Create your views here.
+def index(request):
+	h = httplib2.Http(".cache")
+	resp, content = h.request("http://example.org/", "GET")
+	template = loader.get_template('webapp/index.html')
+
+    return render(request, 'webapp/index.html')
